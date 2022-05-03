a99ffb6d377f19ba067284cdfb7926d61dcd1c47
gilgim <waterather21@gmail.com>
2022-05-03 14:16:51 +0900

    플러팅 버튼 완료

--- RecordingExcerCise/View/CalendarView/FloatingButton.swift
+++ RecordingExcerCise/View/CalendarView/FloatingButton.swift
@@ -10,16 +10,16 @@ import SwiftUI
 
 //  각 인스턴스 식별
 struct ExpandableButtonItem: Identifiable {
+    
     let id = UUID()
     let image: String
     let label: String
-    private(set) var action: (() -> Void)? = nil
 }
 
+//  뷰
 struct ExpandableButtonView: View {
     let primaryItem: ExpandableButtonItem
     let secondaryItems: [ExpandableButtonItem]
-    let none: () -> Void = {}
     let size: CGFloat = 70
     var cornerRadius: CGFloat = 35
     @State var isExpanded = false
@@ -36,8 +36,9 @@ struct ExpandableButtonView: View {
                         HStack{
                             Spacer().frame(width: width*0.8051292)
                             
-                            Button(action: secondaryItems[0].action ?? self.none)
-                            {
+                            NavigationLink {
+                                RoutineView()
+                            } label: {
                                 ZStack{
                                     Circle()
                                     ZStack{
@@ -47,21 +48,23 @@ struct ExpandableButtonView: View {
                                             .foregroundColor(.white)
                                     }
                                     .padding(18)
+                                    .overlay(Text("\(secondaryItems[0].label)").foregroundColor(.white).offset(x: -width*0.144358974358974, y: 0))
                                 }
                             }
                             Spacer().frame(width: 16)
                         }
                         Spacer().frame(height: isShowGray ? height*0.292072322670376 : height*0.091743119266055)
                     }
-                    .transition(.offset(x: 0, y: 140))
+                    .transition(.offset(x: 0, y: height*0.097357440890125*2))
                     
                     VStack{
                         Spacer().frame(height: height*0.721835883171071)
                         HStack{
                             Spacer().frame(width: width*0.8051292)
                             
-                            Button(action: secondaryItems[1].action ?? self.none)
-                            {
+                            NavigationLink {
+                                ExerciseView()
+                            } label: {
                                 ZStack{
                                     Circle()
                                     ZStack{
@@ -71,21 +74,23 @@ struct ExpandableButtonView: View {
                                             .foregroundColor(.white)
                                     }
                                     .padding(18)
+                                    .overlay(Text("\(secondaryItems[1].label)").foregroundColor(.white).offset(x: -width*0.144358974358974, y: 0))
                                 }
                             }
                             Spacer().frame(width: 16)
                         }
                         Spacer().frame(height: height*0.19471488178025)
                     }
-                    .transition(.offset(x: 0, y: 70))
+                    .transition(.offset(x: 0, y: height*0.097357440890125))
                     
                     VStack{
                         Spacer().frame(height: height*0.916550764951321)
                         HStack{
                             Spacer().frame(width: width*0.8051292)
-                            
-                            Button(action: secondaryItems[2].action ?? self.none)
-                            {
+                            NavigationLink {
+                                SettingView()
+                                    .navigationTitle(Text("??"))
+                            } label: {
                                 ZStack{
                                     Circle()
                                     ZStack{
@@ -93,29 +98,29 @@ struct ExpandableButtonView: View {
                                             .resizable()
                                             .scaledToFit()
                                             .foregroundColor(.white)
+                                            
                                     }
                                     .padding(18)
+                                    .overlay(Text("\(secondaryItems[2].label)").foregroundColor(.white).offset(x: -width*0.144358974358974, y: 0))
                                 }
                             }
                             Spacer().frame(width: 16)
                         }
                     }
-                    .transition(.offset(x: 0, y: -70))
+                    .transition(.offset(x: 0, y: -height*0.097357440890125))
                 }
                 
                 //  클릭하는 뷰
                 VStack{
-                    Spacer().frame(height: height*0.829619921363)
+                    Spacer().frame(height: height*0.819193324061196)
                     HStack{
                         Spacer().frame(width: width*0.8051292)
                         
                         Button(action: {
                             withAnimation {
-                                print(height)
                                 self.isExpanded.toggle()
                                 self.isShowGray.toggle()
                             }
-                            self.primaryItem.action?()
                         }) {
                             ZStack{
                                 Circle()
@@ -132,29 +137,24 @@ struct ExpandableButtonView: View {
                         
                         Spacer().frame(width: 16)
                     }
-                    Spacer().frame(height: height*0.091743119266055)
+                    Spacer().frame(height: height*0.097357440890125)
                 }
             }
         }
     }
 }
+
+//  액션
 struct AnimatedExpandableButton: View {
-    @State var text = "none"
     @Binding var isShowGray : Bool
     var body: some View {
         ZStack {
             ExpandableButtonView(
                 primaryItem: ExpandableButtonItem(image: "plus", label:""),
                 secondaryItems: [
-                    ExpandableButtonItem(image: "lock.rotation.open", label: "Lock") {
-                        withAnimation() { self.text = "Lock" }
-                    },
-                    ExpandableButtonItem(image: "xmark.bin", label: "Delete") {
-                        withAnimation() { self.text = "Delete" }
-                    },
-                    ExpandableButtonItem(image: "archivebox", label: "Archive") {
-                        withAnimation() { self.text = "Archive" }
-                    } ], isShowGray: self.$isShowGray )
+                    ExpandableButtonItem(image: "lock.rotation.open", label: "루틴"),
+                    ExpandableButtonItem(image: "xmark.bin", label: "운동") ,
+                    ExpandableButtonItem(image: "archivebox", label: "설정")], isShowGray: self.$isShowGray )
             
         }
     }
