4485f30458479c65451434a5e9c7a71e70cb4779
kimwoojin <waterather21@gaeasoft.co.kr>
2022-06-07 13:54:42 +0900

    일단 중간보고

--- RecordingExcerCise/Model/Exercise.swift
+++ RecordingExcerCise/Model/Exercise.swift
@@ -13,25 +13,51 @@ enum Equipment : String {
     case machine = "머신"
     case nake = "맨몸"
 }
+/**
+ 신체 부위 나누는 것
+ */
+enum BodyPart : String , CaseIterable{
+    case chest = "가슴"
+    case back = "등"
+    case shoulder = "어깨"
+    case lower_Body = "하체"
+    case arm = "팔"
+    case abs = "복근"
+}
 class Exercise : Object{
-    
-    //  운동명
+    /**
+     운동명
+     */
     @Persisted(primaryKey: true) var name : String?
-    
-    //  동작설명
+    /**
+     동작설명
+     */
     @Persisted var explainExcercise : String?
-    
-    //  운동부위
+    /**
+     운동부위
+     */
     @Persisted var exercisePart : String?
-    
-    //  장비
+    /**
+     장비
+     */
     @Persisted var equipment : String?
-    
-    //  운동 유튜브 링크
+    /**
+     운동 유튜브 링크
+     */
     @Persisted var exerciseLink : String?
 }
-
-//  운동부위
-class ExercisePart:Object{
-    
+/**
+    운동만드는 함수 예) 벤치프레스, 부위, 설명 등등
+ */
+func createExercisePart(){
+    let realm = try! Realm()
+    let exercise = Exercise()
+    exercise.name = "벤치프레스"
+    exercise.explainExcercise = "중앙 가슴고립, 어깨 최대한 벌리듯이"
+    exercise.equipment = "바벨"
+    exercise.exercisePart = "중앙 가슴"
+    exercise.exerciseLink = "http://~~~~~"
+    try! realm.write{
+        realm.add(exercise)
+    }
 }
--- RecordingExcerCise/View/ExerciseView/ExerciseView.swift
+++ RecordingExcerCise/View/ExerciseView/ExerciseView.swift
@@ -8,13 +8,101 @@
 import SwiftUI
 
 struct ExerciseView: View {
+    @State var searchText = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Color.Color_13
+            Color.Color_13
+                .edgesIgnoringSafeArea(.bottom)
+            VStack{
+                SearchBar(text: $searchText)
+                    .padding(.top,10)
+                ScrollView(.horizontal, showsIndicators: false){
+                    BodyScrollView()
+                        .padding(.trailing,8)
+                        .padding(.leading,16)
+                }
+                .frame(width: 390, height: 40, alignment: .center)
+                
+                Spacer()
+            }
+        }
     }
 }
 
 struct ExerciseView_Previews: PreviewProvider {
     static var previews: some View {
         ExerciseView()
+    }
+}
+
+struct SearchBar: View {
+    @Binding var text: String
+    var body: some View {
+        HStack {
+            HStack {
+                Image(systemName: "magnifyingglass")
+ 
+                TextField("Search", text: $text)
+                    .foregroundColor(.primary)
+ 
+                if !text.isEmpty {
+                    Button(action: {
+                        self.text = ""
+                    }) {
+                        Image(systemName: "xmark.circle.fill")
+                    }
+                } else {
+                    EmptyView()
+                }
+            }
+            .padding(EdgeInsets(top: 10, leading: 16, bottom: 10, trailing: 16))
+            .foregroundColor(.secondary)
+            .background(.gray.opacity(0.2))
+            .cornerRadius(10.0)
+        }
+        .padding(.horizontal)
+    }
+}
+/**
+    검색바 밑 상단 몸 부위 스크롤 할려고 하는 뷰
+ */
+struct BodyScrollView : View{
+    var body: some View{
+        HStack
+        {
+                BodyPartView(title: "즐겨찾는 운동",width: 130)
+            ForEach(BodyPart.allCases, id:\.self){ i in
+                BodyPartView(title: i.rawValue)
+            }
+        }
+    }
+}
+/**
+    몸 부위 하나를 구성하는 뷰 
+ */
+struct BodyPartView : View{
+    var title : String
+    var width : CGFloat
+    var height : CGFloat
+    init(title:String,width:CGFloat = 55,height:CGFloat = 40){
+        self.title = title
+        self.width = width
+        self.height = height
+    }
+    
+    var body: some View {
+        ZStack{
+            Button{
+                
+            }label: {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                    .overlay(Text("\(title)"))
+            }
+        }
+        .frame(width: width, height: height, alignment: .center)
+        .padding(.trailing,8)
+        
     }
 }
